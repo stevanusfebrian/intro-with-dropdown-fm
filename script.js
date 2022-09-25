1cceeb9f9774cca8108620bbbfcccb8787327ae7
stevanusfebrian <100825866+stevanusfebrian@users.noreply.github.com>
2022-09-25 17:06:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const featureBtn = document.getElementById("feature-button");
+const featureMenu = document.querySelector(".feature-menu");
+const hideFeature = document.querySelector(".hide-feature");
+const rotFeature = document.querySelector(".arrow1");
+
+const companyBtn = document.getElementById("company-button");
+const companyMenu = document.querySelector(".company-menu");
+const hideCompany = document.querySelector(".hide-company");
+const rotCompany = document.querySelector(".arrow2");
+
+if(window.screen.width > 875){
+    // if desktop size
+    featureBtn.addEventListener("click", function(){
+        hideFeature.classList.toggle("hide-feature");
+        rotFeature.classList.toggle("feature-arrow");
+    });
+    
+    companyBtn.addEventListener("click", function(){
+        hideCompany.classList.toggle("hide-company");
+        rotCompany.classList.toggle("company-arrow");
+    });
+}else{
+    // if mobile / tablet size
+    featureBtn.addEventListener("click", function(){
+        hideFeature.classList.toggle("hide-feature");
+        rotFeature.classList.toggle("feature-arrow");
+        featureMenu.classList.toggle("menu-position");
+    });
+    
+    companyBtn.addEventListener("click", function(){
+        hideCompany.classList.toggle("hide-company");
+        rotCompany.classList.toggle("company-arrow");
+        companyMenu.classList.toggle("menu-position");
+    });
+}
+
+// mobile sidebar menu
+const navItems = document.querySelector(".items-container");
+const menu = document.querySelector(".ham-menu");
+const closeMenu = document.querySelector(".close-menu-mobile img");
+const overlay = document.querySelector(".black-overlay");
+
+menu.addEventListener("click", function(){
+    navItems.classList.add("slide");
+    overlay.style.opacity = "1";
+    overlay.style.zIndex = "2";
+});
+
+closeMenu.addEventListener("click", function(){
+    navItems.classList.remove("slide");
+    overlay.style.opacity = "0";
+    overlay.style.zIndex = "-1";
+});
---
+++ style-guide.md
@@ -0,0 +1,27 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Neutral
+
+- Almost White: hsl(0, 0%, 98%) // background color
+- Medium Gray: hsl(0, 0%, 41%) // text and navbar
+- Almost Black: hsl(0, 0%, 8%) // headline & button color
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 18px
+
+### Font
+
+- Family: [Epilogue](https://fonts.google.com/specimen/Epilogue)
+- Weights: 500, 700
